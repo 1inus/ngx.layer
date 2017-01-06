2e1f6e321ae4de252bf182bd7b8500c0524d0d27
lw <cnliangwei@foxmail.com>
2017-01-07 06:13:19 +0800

    license

--- ng2-layer.ts
+++ ng2-layer.ts
@@ -1,4 +1,9 @@
-import { 
+/**
+ * @license Apache License Version 2.0, January 2004 http://www.apache.org/licenses/
+ * @author cnliangwei@foxmail.com
+ */
+
+import {
 	Compiler,
 	Component,
 	NgModule,
